071b671c01302fbf1f1a1fc9ab1157fd15e0da77
statusfailed <sf@statusfailed.com>
2021-01-16 19:12:01 +0000

    Add correlation layers and MNIST model

---
+++ convolutional.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+# TODO: remove this, get mnist another way.
+# import keras
+import numpy as np
+import math
+import mnist
+
+import numeric_optics.lens as lens
+from numeric_optics.para import Para, dense, relu, sigmoid, to_para, initialize_glorot
+from numeric_optics.train import TrainableModel, train, accuracy
+import numeric_optics.convolution as image
+
+# Glorot Uniform initialization for 3x3x1 correlation kernel
+limit_a = math.sqrt(6 / (3*3 + 1))
+a = np.random.uniform(-limit_a, limit_a, ((3,3,3,1)))
+
+# Glorot Uniform initialization for 5x5x3 correlation kernel
+limit_b = math.sqrt(6 / (4*4*3 + 1))
+b = np.random.uniform(-limit_b, limit_b, ((5,4,4,3)))
+
+model = Para(a, image.multicorrelate) \
+     >> relu \
+     >> to_para(image.max_pool_3d(2, 2)) \
+     >> Para(b, image.multicorrelate) \
+     >> relu \
+     >> to_para(image.max_pool_3d(2, 2)) \
+     >> to_para(image.flatten) \
+     >> dense((5*5*5, 10), activation=lens.sigmoid)
+
+if __name__ == "__main__":
+    num_classes = 10
+
+    # Load MNIST data
+    x_train = mnist.train_images()
+    y_train = mnist.train_labels()
+    x_test = mnist.test_images()
+    y_test = mnist.test_labels()
+
+    # Normalize data
+    x_train = x_train.astype('float32') / 255
+    x_test  = x_test.astype('float32') / 255
+
+    # one-hot-encode labels
+    y_train = np.identity(num_classes)[y_train]
+    y_test  = np.identity(num_classes)[y_test]
+
+    # Reshape images to (28, 28, 1) - 28x28 pixels with a single color channel.
+    x_train = x_train.reshape(x_train.shape + (1,))
+    x_test  = x_test.reshape(x_test.shape + (1,))
+
+    # Train with mean squared error and learning rate 0.01
+    trainable = TrainableModel(
+        model=model,
+        update=lens.update(0.01),
+        displacement=lens.mse,
+        inverse_displacement=lens.identity) # TODO: fix
+
+    # Print diagnostics while training
+    e_prev = None
+    fwd    = trainable.model.arrow.fwd
+    for e, j, i, param in train(trainable, x_train, y_train, num_epochs=4, shuffle_data=True):
+        if j % 10000:
+            continue
+
+        e_prev = e
+        f = lambda x: fwd((param, x)).argmax()
+        # NOTE: this is *TEST* accuracy, unlike iris experiment.
+        acc = accuracy(f, x_test, y_test.argmax(axis=1))
+        print('epoch', e, 'sample', j, '\taccuracy {0:.4f}'.format(acc), sep='\t')
+
+    # final accuracy
+    acc = accuracy(f, x_test, y_test.argmax(axis=1))
+    print('final accuracy: {0:.4f}'.format(acc))
---
+++ numeric_optics/convolution.py
@@ -0,0 +1,186 @@
+import scipy.signal as signal
+import scipy.ndimage as ndimage
+import numpy as np
+
+from numeric_optics.lens import Lens
+
+################################################################
+# Convolution
+################################################################
+
+# Convolve a single 2D kernel (k) over a 2D image (a)
+# args: (k, a), where k is the filter kernel and a is an image
+def convolve2d_fwd(args):
+    k, a = args
+    return signal.correlate2d(a, k, mode='valid')
+
+# Reverse derivative of 2D convolution.
+def convolve2d_rev(args):
+    (k, a), dy = args
+    dk = signal.correlate2d(a, dy, mode='valid')
+    da = np.flip(signal.correlate2d(np.flip(k), dy, boundary='fill', fillvalue=0))
+    return (dk, da)
+
+convolve2d = Lens(convolve2d_fwd, convolve2d_rev)
+
+################################################################
+# Flattening, Pooling
+################################################################
+
+# Flatten an array
+# NOTE: this is a "type-level" operation: nothing about the array data is
+# changed, so we should have the property that
+# >>> x == flatten.rev(x, flatten.fwd(x))
+def unflatten(args):
+    x, dy = args
+    return np.reshape(dy, x.shape)
+
+# The "flatten" lens simply changes the type of the input
+flatten = Lens(np.ndarray.flatten, unflatten)
+
+# Max pooling layer with kernel width, height of (kh, kw)
+def max_pool_2d(kh, kw):
+    def max_pool_fwd(m):
+        # note: assumes 2D array
+        h, w = m.shape
+        mh = h // kh
+        mw = w // kw
+        
+        return m[:mh * kh, :mw * kw].reshape(mh, kh, mw, kw).max(axis=(1,3))
+
+    def max_pool_rev(args):
+        m, dm = args
+        n = max_pool_fwd(m)
+        # Expand the size of the output vector so each item is repeated (kh x kw) times.
+        n_rep  = n.repeat(kh, axis=0).repeat(kw, axis=1)
+        dm_rep = dm.repeat(kh, axis=0).repeat(kw, axis=1)
+        result = (m == n_rep) * dm_rep
+        check = result.shape == m.shape
+        return result
+
+    return Lens(max_pool_fwd, max_pool_rev)
+
+################################################################
+# Convolutional layers with channels
+# TODO: vectorise the functions in this section
+################################################################
+
+# Correlate a 3D kernel volume over a 3D image with the same depth.
+# Inputs:
+#   k:  (Kw, Kh,  c)
+#   a:  ( w,  h,  c)
+# Outputs:
+#   y:  (w - Kw + 1,  h - Kh + 1)
+def correlate_volume_fwd(k, a):
+    kw, hw, kc = k.shape
+    w,  h,  c  = a.shape
+    if c != kc:
+        raise ValueError("image channels {} != {} kernel channels".format(c, kc))
+
+    rs = []
+    for i in range(0, c):
+        ker = k[:, :, i]
+        img = a[:, :, i]
+        rs.append(convolve2d_fwd((ker, img)))
+    return np.sum(rs, axis=0)
+
+# Reverse derivative of 3D correlation
+# Inputs:
+#   k:  (Kw, Kh,  c)
+#   a:  ( w,  h,  c)
+#   dy: (w - Kw + 1, h - Kh + 1)
+# Outputs:
+#   dks: (Kw, Kh, c)
+#   das: ( w,  h, c)
+def correlate_volume_rev(k, a, dy):
+    kw, hw, kc = k.shape
+    w,  h,  c  = a.shape
+    if c != kc:
+        raise ValueError("image channels {} != {} kernel channels".format(c, kc))
+
+    dks = []
+    das = []
+    for i in range(0, c):
+        ker = k[:, :, i]
+        img = a[:, :, i]
+        dk, da = convolve2d_rev(((ker, img), dy))
+        dks.append(dk)
+        das.append(da)
+
+    # Move the channel axis to rightmost position
+    dks = np.moveaxis(np.array(dks), 0, -1)
+    das = np.moveaxis(np.array(das), 0, -1)
+    return dks, das
+
+# Correlate multiple 3D volumes over a 3D input image
+# Inputs:
+#   ks: (NK, Kw, Kh,  c)
+#   a:      ( w,  h,  c)
+# Outputs:
+#   ys: (w - Kw + 1, h - Kh + 1, NK)
+def multicorrelate_volume_fwd(ks, a):
+    nk, _, _, _ = ks.shape
+    acc = []
+    # Correlate each filter with the volume "a"
+    for i in range(0, nk):
+        k = ks[i]
+        acc.append(correlate_volume_fwd(k, a))
+    return np.moveaxis(np.array(acc), 0, -1)
+
+# Reverse derivative of multicorrelate_volume_fwd
+# Inputs:
+#   ks: (NK, Kw, Kh,  c)
+#   a:      ( w,  h,  c)
+#   ys: (w - Kw + 1, h - Kh + 1, NK)
+# Outputs:
+#   dks: (NK, Kw, Kh, c)
+#   da:     ( w,  h,  c)
+def multicorrelate_volume_rev(ks, a, dys):
+    nk, kh, kw, kc = ks.shape
+    w, h, c = a.shape
+    yw, yh, yc = dys.shape
+    if kc != c:
+        raise ValueError("Kernel channels != image channels")
+    if nk != yc:
+        raise ValueError("Number of kernels != number of output channels")
+
+    dkss = []
+    da = np.zeros(a.shape)
+    for i in range(0, nk):
+        k  = ks[i]
+        dy = dys[:, :, i]
+        dks, das = correlate_volume_rev(k, a, dy)
+        da += das
+        dkss.append(dks)
+    return np.array(dkss), da
+
+multicorrelate = Lens(
+    lambda ksa: multicorrelate_volume_fwd(ksa[0], ksa[1]), 
+    lambda kay: multicorrelate_volume_rev(kay[0][0], kay[0][1], kay[1]))
+
+
+# Max Pooling for images with channels
+def max_pool_3d(kh, kw):
+    lens = max_pool_2d(kh, kw)
+
+    def max_pool_3d_fwd(m):
+        # note: assumes 2D array
+        h, w, c = m.shape
+
+        acc = np.zeros((h // kh, w // kw, c))
+        for i in range(0, c):
+            acc[:, :, i] += lens.fwd(m[:, :, i])
+
+        return acc
+
+    def max_pool_3d_rev(args):
+        x, dy = args
+        h, w, c = x.shape
+
+        dx = np.zeros(x.shape)
+        for i in range(0, c):
+            dx[:, :, i] = lens.rev((x[:, :, i], dy[:, :, i]))
+
+        return dx
+
+    return Lens(max_pool_3d_fwd, max_pool_3d_rev)
